e4254295cd596a1cd2735337fc826a841f0d9082
malekqs <57891973+malekqs@users.noreply.github.com>
2021-10-27 01:54:35 +0300

    ..

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-require('dotenv')?.config();
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv')?.config();
+}
 
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); 
 
-mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true }) 
+mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true })
 
 const db = mongoose.connection;
 db.on('error', (e) => console.error('db error: ', e));
-db.once('open', () => console.log('conncted to db')) 
+db.once('open', () => console.log('conncted to db'))
 
 app.use(express.json());
 
 const subscribersRouter = require('./routes/subscribers');
 app.use('/subscribers', subscribersRouter);
 
-app.listen(process.env.PORT || 3000, () => console.log('server started')); 
+app.listen(process.env.PORT || 3000, () => console.log('server started'));
